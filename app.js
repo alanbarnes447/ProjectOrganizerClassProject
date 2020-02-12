befbb598479c95b379f2b65e58518928730e5e58
Ric Dominguez <hacker846@test.com>
2020-02-12 06:37:09 -0600

    Updated views and added routes for about page and team page

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routers added
+var aboutRouter = require('./routes/about');
+var ourTeamRouter = require('./routes/our-team');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Ref. to routers added
+app.use('/about', aboutRouter);
+app.use('/our-team', ourTeamRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/our-team.js
+++ routes/our-team.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET 'Our Team' page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('our-team');
 });
 
 module.exports = router;
