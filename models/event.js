b6ef3302a7f3c1a9b713c292f2fa4a2d5cdc8317
Ric Dominguez <hacker846@test.com>
2020-04-06 10:08:43 -0500

    We can now add events to our individual calendars.

--- models/event.js
+++ models/event.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-  USER_IDENTIFIER: { // Identifier created by MongoBD.
+  email: { // Identifier created by MongoBD.
     type: String,
     required: true
   },
-  date: {
+  title: {
+    type: String,
+    required: true
+  },
+  start: {
     type: Date,
     required: true
+  },
+  end: {
+    type: Date,
+    required: false
+  },
+  allDay: {
+    type: Boolean,
+    required: true
   }
-});
+}, { versionKey: false });
 
 const Event = mongoose.model('Event', EventSchema);
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,16 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+const Event = require('../models/event');
+
 const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('dashboard', {
-    user: req.user
-  });
+  var events = [];
+  
+  Event.find({
+      email: req.user.email
+    }, {
+      _id: 0
+    })
+    .stream()
+    .on('data', function(event) {
+      delete event.email
+      events.push(event)
+    })
+    .on('error', function(err) {
+      res.send('Error retrieving event(s).');
+    })
+    .on('end', function() {
+      console.log(events);
+
+      res.render('dashboard', {
+        user: req.user,
+        event: events
+      });
+    })
 });
 
-router.post('/', function(req, res, next) {
+router.post('/add-event', function(req, res, next) {
+  const { email, title, start, allDay } = req.body;
 
+  const newEvent = new Event({
+    email,
+    title,
+    start,
+    allDay
+  });
+
+  newEvent.save()
+    .then(user => {
+      res.redirect('/')
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
